e988a3f586c357808912b5eea49c0fdc5e491cf4
soham janbandhu <96688838+SOHAMJANBANDHU@users.noreply.github.com>
2023-06-28 19:51:15 +0530

    Scan.java

--- practice/Scan.java
+++ practice/Scan.java
@@ -4,17 +4,21 @@ class Scan
 	public static void main(String[] args) 
 	{
 		Scanner scan = new Scanner(System.in);
-		
+
+		System.out.println("Enter int");
 		int i = scan.nextInt();
-        double d = scan.nextDouble();
+		System.out.println("Enter double");
+        	double d = scan.nextDouble();
 		scan.nextLine();
-        String s = scan.nextLine();
+		System.out.println("Enter String");
+        	String s = scan.nextLine();
+		System.out.println("Enter int");
 		int k = scan.nextInt();
 
 		System.out.println("Int: " + k);
 		System.out.println("String: " + s);
-        System.out.println("Double: " + d);
-        System.out.println("Int: " + i);
+       	 	System.out.println("Double: " + d);
+        	System.out.println("Int: " + i);
     
 	}
 }
